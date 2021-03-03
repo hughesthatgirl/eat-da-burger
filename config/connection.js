f24993526000431158d09df9d3e3a321026d9aad
JoannaChandler <jhazimih@gmail.com>
2021-03-02 22:13:37 -0500

    Updating connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: 'jhsqlpass1006!',
-  database: 'burgers_db',
-});
+let connection;
 
 // Make connection.
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'jhsqlpass1006!',
+    database: 'burgers_db',
+  });
+}
+     
 connection.connect((err) => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
